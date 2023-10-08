615ca3651a649f0593a889b6acc2297fa187cef2
fabiosleal2712 <lymaleal@gmail.com>
2023-10-08 02:27:45 -0300

    add Nui.Linving

---
+++ app/wwwroot/js/script.js
@@ -0,0 +1,15 @@
+﻿
+window.blazorResize = {
+    registerResizeCallback: function (dotnetHelper) {
+        window.addEventListener('resize', () => {
+            dotnetHelper.invokeMethodAsync('OnBrowserResize', window.innerWidth);
+        });
+    }
+};
+
+window.getDimensions = function () {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+};
